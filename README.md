707073fea9d743ba841675c2498da4a611e9d342
carlsimpson <csimpson.web@gmail.com>
2016-08-09 15:33:00 +0100

    site layout structure added to readme.md

--- README.md
+++ README.md
@@ -1,3 +1,84 @@
 # personalsite
-transfer repo for the new site build
 
+Updated personal website for 2016: 
+
+#Homepage
+
+Edits Required
+
+* 3 boxes on homepage to display latest & best content
+* 
+
+#Bio
+
+Edits Required
+
+* Photo in place
+* Required Blurb
+
+#Games
+
+Edits Required
+
+* Gamepage has been updated - needs visually addressing
+* 
+
+#Code Projetcs
+
+Edits Required
+
+* Actual code projects featuring ALL necessary upto date techs
+* Angular2 - Laravel - Famous - Phaser open source structure
+
+#My Work
+
+Edits Required
+
+*
+*
+
+
+#CV
+
+Edits Required
+
+* copy from OG site needs displaying in better visual format using bootsrap tools.
+* lines graphs e.t.c
+
+* [ Apps4 ]
+* AR experiences 17 augmented relaity "experiences" geolocated to positions in wales ( telling indiviual historical stories using AR triggers / games / videos ) testing/site testing - ProjectManagement/Client Liason. famo.us/phonegap
+* 17 phaser games built in a infant stage library which i had to learn.
+* Phaser template design - opensource release
+* TenACRE GAMES x 4 - format design / concepts / builds. 2 games live onsite over wimbledon/rio olympics.
+* VR christmas demo in UNITY for Oculus demo
+* APPS - loyalty platform
+* HULL IR experience - build - testing/site testing - ProjectManagement/Client Liason.
+* GEOTASK, build - research and testing/site testing - implementing of ibeacons - installation in towns.
+* Wordpress Websites - love-letchworth , love-sutton - ProjectManagement/Client Liason.
+* Ecommerce - tapuach.
+* POC CLient Liason for various projects.
+
+* [ Blumin ]
+* Email Marketing builds and campaigns.
+* website builds.
+
+* [ Cuckoo ]
+* company legacy website content management.
+* junior dev tasks.
+
+* [ SchoolSpider ]
+* HTML5/CSS3 template website builds on a bespoke php platform for schoolspider.
+
+#Contact
+
+Edits Required
+
+* contact form
+* email contact platform.
+
+#SiteWide
+
+Edits Required
+
+* Edit Menu to change to active on each click
+* Visual Improvement
--- game.php
+++ game.php
@@ -14,21 +14,22 @@
 	  <div class="col-sm-4"><a href="games/KernelKatcha"><img src="img/gamescreen/kernelkatcha.png" width="235" height="235"/></a></div>
 	  <div class="col-sm-4"><a href="http://www.indestinate.com/prod/bridgend/PorthcawlNavigator/"><img src="img/gamescreen/navigator_gameimage.png" /></a></div>
 	</div>
-    <div class="row">
+    <!--<div class="row">
 	  <div class="col-sm-4"><a href="http://www.indestinate.com/prod/bridgend/SheepDip/"><img src="img/gamescreen/sheepdip_gameimage.png" /></a></div>
 	  <div class="col-sm-4"><a href="http://www.indestinate.com/prod/bridgend/Firepit/"><img src="img/gamescreen/firepit_gameimage.png" /></div>
 	  <div class="col-sm-4"><a href="http://www.indestinate.com/prod/bridgend/Ghosthunter/"><img src="img/gamescreen/ghost_gameimage.png" /></div>
-	</div>
+	</div>-->
     <div class="row">
+    <div class="col-sm-4"><a href="games/astroLander"><img src="img/gamescreen/astroLander.png" alt="..." width="235" height="235" /></a></div>
 	  <div class="col-sm-4"><a href="games/ld35"><img src="img/gamescreen/ld35_gameimage.png" alt="..." width="235" height="235" /></a></div>
-	  <div class="col-sm-4"><a href="games/pixel"><img src="img/gamescreen/pixel_gameimage.png" alt="..." /></a></div>
 	  <div class="col-sm-4"><a href="games/theFinalFrontier"><img src="img/gamescreen/finalFrontier_gameimage.png" alt="..." /></a></div>
 	</div>
-    <div class="row">
-	  <div class="col-sm-4"><a href="games/astroLander"><img src="img/gamescreen/astroLander.png" alt="..." width="235" height="235" /></a></div>
+    <!--<div class="row">
+	  <div class="col-sm-4"><a href="games/pixel"><img src="img/gamescreen/pixel_gameimage.png" alt="..." /></a></div>
 	  <div class="col-sm-4"><a href="games/CaveCopter"><img src="img/gamescreen/copter_gameimage.png" alt="..." /></a></div>
 	  <div class="col-sm-4"><a href="games/CaveExplorer"><img src="img/gamescreen/cave_gameimage.png" alt="..." /></a></div>
 	</div>
+	-->
 	<!--
     <div class="row">
 	  <div class="col-sm-4"><a href="games/"><img src="img/gamescreen/.png" alt="..." width="235" height="235" /></a></div>
